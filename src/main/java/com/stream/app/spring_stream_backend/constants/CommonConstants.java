9cdf05cf41e57100a68b80fcf88c1cc1b8d8812c
Jagman0477 <76586290+Jagman0477@users.noreply.github.com>
2025-01-11 20:39:52 +0530

    Stream changes for fetching from specific start point.

--- src/main/java/com/stream/app/spring_stream_backend/constants/CommonConstants.java
+++ src/main/java/com/stream/app/spring_stream_backend/constants/CommonConstants.java
@@ -4,6 +4,7 @@ public class CommonConstants {
 
     public static final String VIDEO_SAVED_SUCCESSFULLY = "Video saved successfully";
     public static final String VIDEO_NOT_UPLOADED = "Video not uploaded successfully";
+    public static final String VIDEO_RANGE_NOT_FETCHED = "Video data couldn't be fetched";
     public static final String SUCCESS = "Success";
     public static final String FAILURE = "Failure";
     public static final String IO_EXCEPTION_MESSAGE = "Failed because of IO Exception.";
